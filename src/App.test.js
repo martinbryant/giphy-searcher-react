1d3ce2de6cf3cc3353f7efc42b1b111d5e2dd881
Martin Bryant <mail@martinbryant.me>
2018-04-01 15:09:25 +0100

    Added Expect & Enzyme ready for TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import expect from 'expect';
+
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+const component = () => shallow(<App />);
+
+describe('App component tests', () => {
+  it('renders a div', () => {
+    expect(component().find('div').length).toBe(1);
+  });
+  it('with a class of "container" ', () => {
+    expect(component().find('div').hasClass('container')).toBe(true);
+  });
+
+
+  // ReactDOM.unmountComponentAtNode(div);
 });
