deeee2b95a239fe539eff24c557ec635d23ee34a
Martin Bryant <mail@martinbryant.me>
2018-05-20 17:46:30 +0100

    Failing loadingComponent test

--- src/components/loading-component.test.js
+++ src/components/loading-component.test.js
@@ -6,9 +6,27 @@ import LoadingComponent from './loading-component';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Loading Component tests', () => {
+    const setup = (setupProps) => shallow(<LoadingComponent {...setupProps} />);
     it('renders a div with a class name of loading-section', () => {
-        const component = shallow(<LoadingComponent />)
-        const firstDiv = component.find('div').first()
-        expect(firstDiv.hasClass('loading-section')).toBe(true);
+        const component = setup();
+        const expected = component.find('div').first()
+        expect(expected.hasClass('loading-section')).toBe(true);
+    });
+    it('renders load more button if load was successful', () => {
+        const component = setup({
+            loadedGifList: ['https://media3.giphy.com/media/39qyWO7EM4Ov3fjyuj/200_d.gif',
+                'https://media3.giphy.com/media/39qyWO7EM4Ov3fjyuj/200_d.gif'],
+            loading: false,
+            loadingError: false
+        });
+        const expected = component.find('.load-more-button');
+        expect(expected.exists()).toBe(true);
     })
+    it('does not render load more button if loading')
+    it('does not render load more button when there is a loading error')
+    it('renders a spinner if loading')
+    it('does not render a spinner if not loading')
+    it('renders an error if there is a loading error')
+    it('does not render an error if there is no error')
+
 })
