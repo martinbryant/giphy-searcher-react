c5f4890283bd5da14c0a15498c4abf190591efa8
Martin Bryant <mail@martinbryant.me>
2018-05-20 21:05:05 +0100

    Tests for loadingComponent refactored and added behaviour

--- src/components/loading-component.js
+++ src/components/loading-component.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const LoadingComponent = ({ loading, loadingError, loadedGifList }) => {
+const LoadingComponent = ({ loading, loadingError, loadedGifList, loadMoreGifs }) => {
     return (
         <div className="loading-section">
-            {(loadedGifList.length > 0 && !loading && !loadingError) && <div className="load-more-button"></div>}
-            {loading && <div className="spinner fas fa-spinner fa-spin fa-4x"></div>}
-            {loadingError && <div className="error-message"></div>}
+            {(loadedGifList.length > 0 && !loading && !loadingError)
+                && <button className="load-more-button" onClick={loadMoreGifs}>Load More</button>}
+            {loading && <i className="spinner fas fa-spinner fa-spin fa-4x"></i>}
+            {loadingError && <div className="error-message">{loadingError}</div>}
         </div>
     )
 }
--- src/components/loading-component.test.js
+++ src/components/loading-component.test.js
@@ -9,18 +9,24 @@ describe('Loading Component tests', () => {
     const setup = (setupProps) => shallow(<LoadingComponent {...setupProps} />);
     it('renders a div with a class name of loading-section', () => {
         const component = setup({ loadedGifList: [], loadingError: '' });
-        const expected = component.find('div').first()
-        expect(expected.hasClass('loading-section')).toBe(true);
+        const expected = component.find('.loading-section')
+        expect(expected.exists()).toBe(true);
     });
     it('renders load more button if load was successful', () => {
+        const loadMoreGifs = jest.fn();
         const component = setup({
             loadedGifList: ['https://media3.giphy.com/media/39qyWO7EM4Ov3fjyuj/200_d.gif',
                 'https://media3.giphy.com/media/39qyWO7EM4Ov3fjyuj/200_d.gif'],
             loading: false,
-            loadingError: ''
+            loadingError: '',
+            loadMoreGifs
         });
         const expected = component.find('.load-more-button');
         expect(expected.exists()).toBe(true);
+        expect(expected.is('button')).toBe(true)
+        expect(expected.text()).toBe('Load More')
+        expected.simulate('click');
+        expect(loadMoreGifs).toHaveBeenCalled()
     })
     it('does not render load more button if loading', () => {
         const component = setup({
@@ -78,6 +84,7 @@ describe('Loading Component tests', () => {
         });
         const expected = component.find('.error-message');
         expect(expected.exists()).toBe(true);
+        expect(expected.text()).toBe('The returned network error')
     })
     it('does not render an error if there is no error', () => {
         const component = setup({
