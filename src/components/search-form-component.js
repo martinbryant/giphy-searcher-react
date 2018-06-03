16b44ad79e6ba2ffd89566395e0eab5c5f172170
Martin Bryant <mail@martinbryant.me>
2018-06-03 19:03:46 +0100

    Passing SearchFormComponent test

--- src/components/search-form-component.js
+++ src/components/search-form-component.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SearchFormComponent = ({ submitSearch }) => {
+const SearchFormComponent = ({ searchError, submitSearch }) => {
     return (
         <div className="search">
             <form className="search-form" onSubmit={submitSearch}>
                 <input className="search-input" type="text" />
                 <input className="search-button" type="submit" />
             </form>
-            <div className="search-error">
+            <div className="search-error">{searchError}
             </div>
         </div>
     )
