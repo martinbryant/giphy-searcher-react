bab94cfbc6a21c3e2ea1e992be401846f1f03c7a
Martin Bryant <mail@martinbryant.me>
2018-06-03 17:59:08 +0100

    Passing ResultDisplay test

--- src/components/result-display.js
+++ src/components/result-display.js
@@ -2,7 +2,8 @@ import { connect } from 'react-redux';
 
 import ResultDisplayComponent from './result-display-component';
 
-const mapStateToProps = () => ({
+const mapStateToProps = ({ loadedGifList }) => ({
+    loadedGifList
 })
 
 export const ResultDisplay = connect(mapStateToProps)(ResultDisplayComponent);
