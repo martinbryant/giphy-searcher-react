d561aee13d7357d107654197cd98b84099979c23
Martin Bryant <mail@martinbryant.me>
2018-06-03 21:45:17 +0100

    Passing SearchFormComponent test

--- src/components/search-form-component.js
+++ src/components/search-form-component.js
@@ -8,7 +8,7 @@ const SearchFormComponent = ({ searchError, submitSearch }) => {
                 submitSearch(e.target.value);
             }}>
                 <input className="search-input" type="text" />
-                <input className="search-button" type="submit" />
+                <input className="search-button" type="submit">Search Giphy</input>
             </form>
             {searchError &&
                 <div className="search-error">{searchError}
