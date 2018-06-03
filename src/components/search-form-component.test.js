4fc6696f63349700128c3c3b2eca6b6556bc3d01
Martin Bryant <mail@martinbryant.me>
2018-06-03 18:29:59 +0100

    Failing SearchFormComponent test

--- src/components/search-form-component.test.js
+++ src/components/search-form-component.test.js
@@ -11,10 +11,12 @@ describe('SearchForm Component tests', () => {
         const component = setup({});
         const expected = component.find('.search')
         expect(expected.exists()).toBe(true);
+        expect(expected.is('div')).toBe(true);
     })
-    it('renders a div with a class name of search-form', () => {
+    it('renders a form with a class name of search-form', () => {
         const component = setup({});
         const expected = component.find('.search-form')
         expect(expected.exists()).toBe(true);
+        expect(expected.is('form')).toBe(true)
     })
 })
