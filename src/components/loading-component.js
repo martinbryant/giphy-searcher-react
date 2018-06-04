26adfaa5041009d3939f34c09db28af0dfaeee9c
Martin Bryant <mail@martinbryant.me>
2018-06-04 21:42:29 +0100

    Passing LoadingComponent test

--- src/components/loading-component.js
+++ src/components/loading-component.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const LoadingComponent = ({ loading, loadingError, loadedGifList, loadMoreGifs }) => {
+const LoadingComponent = ({ loading, loadingError, loadedGifList, loadMoreGifs, isTrendingSearch }) => {
     return (
         <div className="loading-section">
-            {(loadedGifList.length > 0 && !loading && !loadingError)
+            {(loadedGifList.length > 0 && !loading && !loadingError && !isTrendingSearch)
                 && <button className="load-more-button" onClick={loadMoreGifs}>Load More</button>}
             {loading && <i className="spinner fas fa-spinner fa-spin fa-4x"></i>}
             {loadingError && <div className="error-message">{loadingError}</div>}
