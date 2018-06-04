ac93c23f58ad55b1a8e4c505d05ed1fbce3df153
Martin Bryant <mail@martinbryant.me>
2018-06-04 21:35:29 +0100

    Failing Loading test

--- src/components/loading.test.js
+++ src/components/loading.test.js
@@ -2,22 +2,6 @@ import * as container from './loading';
 import { getMoreGifsStarted } from '../actions/actions';
 
 describe('Loading tests', () => {
-    it('mapStateToProps should return the correct props from state', () => {
-        const state = {
-            searchTerm: '',
-            searchError: '',
-            gifsRequired: 5,
-            loadedGifList: [],
-            loadingError: '',
-            loadingStatus: false
-        }
-        const expected = {
-            loadedGifList: [],
-            loadingError: '',
-            loadingStatus: false
-        }
-        expect(container.mapStateToProps(state)).toEqual(expected);
-    });
     it('mapStateToProps should return isTrendingSearch true when empty searchTerm', () => {
         const state = {
             searchTerm: '',
@@ -32,6 +16,23 @@ describe('Loading tests', () => {
             loadingError: '',
             loadingStatus: false,
             isTrendingSearch: true
+        }
+        expect(container.mapStateToProps(state)).toEqual(expected);
+    })
+    it('mapStateToProps should return isTrendingSearch false when any searchTerm', () => {
+        const state = {
+            searchTerm: 'search',
+            searchError: '',
+            gifsRequired: 5,
+            loadedGifList: [],
+            loadingError: '',
+            loadingStatus: false
+        }
+        const expected = {
+            loadedGifList: [],
+            loadingError: '',
+            loadingStatus: false,
+            isTrendingSearch: false
         }
         expect(container.mapStateToProps(state)).toEqual(expected);
     })
