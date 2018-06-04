e1d1c974a016ce50b8782017a20c4e5fe672ca18
Martin Bryant <mail@martinbryant.me>
2018-06-04 20:01:39 +0100

    Refactored configureStore to include searchMiddleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 
 import reducer from '../reducers/reducer';
-import giphyApi from '../api/giphyApi';
+import { searchMiddleware } from '../middleware/middleware';
 
 const middleware = composeWithDevTools(applyMiddleware(
-    thunk.withExtraArgument(giphyApi),
-    reduxImmutableStateInvariant()
+    reduxImmutableStateInvariant(),
+    searchMiddleware
 ))
 
 export default function configureStore(initialState) {
-    return createStore(reducer, initialState, middleware());
+    return createStore(reducer, initialState, middleware);
 }
