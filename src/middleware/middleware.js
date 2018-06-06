6df1b88aa551ac26ad4ba0c01f7a6c2ff7cc0c25
Martin Bryant <mail@martinbryant.me>
2018-06-06 21:02:19 +0100

    Refactored middleware for all actions to hit store first + remove if's

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,31 +1,45 @@
 import * as actions from '../actions/actions';
 
 export const searchMiddleware = ({ dispatch, getState }) => next => action => {
-    const getFromApiWithStore = getFromApi(dispatch, getState());
-    if (action.type === 'SUBMIT_SEARCH') {
-        let searchError = validateSearchTerm(action.searchTerm)
-        searchError
-            ? dispatch(actions.submitSearchError(searchError))
-            : dispatch(actions.submitSearchSuccess(action.searchTerm))
-    }
     next(action);
 
-    (action.type === 'SUBMIT_SEARCH_SUCCESS') &&
-        dispatch(actions.getNewGifsStarted(action.searchTerm));
+    const getFromApiWithStore = getFromApi(dispatch, getState());
 
-    if (action.type === 'GET_NEW_GIFS_STARTED') {
-        const { getNewGifsSuccess, getNewGifsFailure } = actions;
-        return getFromApiWithStore(getNewGifsSuccess, getNewGifsFailure)
-    }
-    if (action.type === 'GET_MORE_GIFS_STARTED') {
-        const { getMoreGifsSuccess, getMoreGifsFailure } = actions;
-        return getFromApiWithStore(getMoreGifsSuccess, getMoreGifsFailure)
-    }
-    if (action.type === 'GET_TRENDING_GIFS_STARTED') {
-        const { getTrendingGifsSuccess, getTrendingGifsFailure } = actions;
-        return getFromApiWithStore(getTrendingGifsSuccess, getTrendingGifsFailure);
+    switch (action.type) {
+        case 'SUBMIT_SEARCH':
+            {
+                let searchError = validateSearchTerm(action.searchTerm)
+                searchError
+                    ? dispatch(actions.submitSearchError(searchError))
+                    : dispatch(actions.submitSearchSuccess(action.searchTerm))
+                break;
+            }
+        case 'SUBMIT_SEARCH_SUCCESS':
+            {
+                dispatch(actions.getNewGifsStarted(action.searchTerm));
+                break;
+            }
+
+        case 'GET_NEW_GIFS_STARTED':
+            {
+                const { getNewGifsSuccess, getNewGifsFailure } = actions;
+                return getFromApiWithStore(getNewGifsSuccess, getNewGifsFailure)
+            }
+        case 'GET_MORE_GIFS_STARTED':
+            {
+                const { getMoreGifsSuccess, getMoreGifsFailure } = actions;
+                return getFromApiWithStore(getMoreGifsSuccess, getMoreGifsFailure)
+            }
+        case 'GET_TRENDING_GIFS_STARTED':
+            {
+                const { getTrendingGifsSuccess, getTrendingGifsFailure } = actions;
+                return getFromApiWithStore(getTrendingGifsSuccess, getTrendingGifsFailure);
+            }
+        default: break
     }
 }
+
+const validateSearchTerm = searchTerm => searchTerm === '' ? 'Search term cannot be blank!' : ''
 
 const gifResponseToGifUrlList = gifResponse => {
     try {
@@ -34,8 +48,6 @@ const gifResponseToGifUrlList = gifResponse => {
         return err
     }
 }
-
-const validateSearchTerm = searchTerm => searchTerm === '' ? 'Search term cannot be blank!' : ''
 
 const calculateGifUrl = (limit, searchTerm, offset) => {
     const searchUrl = 'http://api.giphy.com/v1/gifs/search?q=';
